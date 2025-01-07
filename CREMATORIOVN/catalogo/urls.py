a9a9977b7fbe8fb1fce64e28ff2c1891b1706d19
RenatoYnonan <renatoignite1122@gmail.com>
2025-01-07 11:04:15 -0500

    Nuevo Csmbio 07-01-25

--- CREMATORIOVN/catalogo/urls.py
+++ CREMATORIOVN/catalogo/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from  .views import *
 
 urlpatterns = [
+    #LISTAR PRODUCTOS
     path('list-product/', ListProductos, name='list-product' ),
-    path('create-product/', CreateProduct, name='create-product')
+    #CREAR PRODCUTOS
+    path('create-product/', CreateProduct, name='create-product'),
+    #ACTUALIZAR PRODUCTO
+    path('update-product/<int:id>', UpdateProduct, name='update-product'),
+    #ELIMINAR PRODUCTOS
+    path('delete-product/<int:id>', DeleteProduct, name='delete-product'),
 ]
--- CREMATORIOVN/catalogo/views.py
+++ CREMATORIOVN/catalogo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import *
 
 from django.http import HttpResponseRedirect, HttpResponse
@@ -22,6 +22,36 @@ def CreateProduct(request):
     else:
         form = FormsProduct()
     return render(request, 'productos/index-form.html', { 'form': form })
+
+
+def UpdateProduct(request, id):
+    datos = get_object_or_404(ModelsProduct, id=id)
+
+    if request.method == 'POST':
+        form = FormsProduct(request.POST, instance=datos)
+
+        if form.is_valid():
+            form.save()
+            return redirect('catalogo:list-product')
+    else:
+        form = FormsProduct(instance=datos)
+
+
+    return render(request, 'productos/index-form.html', {'form': form})
+
+
+def DeleteProduct(request, id):
+    data = get_object_or_404(ModelsProduct, id=id)
+
+    if request.method == 'POST':
+        data.state_product = False
+        data.save()
+        return redirect('catalogo:list-product')
+    else:
+        form = FormsProduct(instance=data)
+    
+    return render(request, 'productos/index-delete.html', {'form': form})
+    
     
 
 class ListPlanes():
