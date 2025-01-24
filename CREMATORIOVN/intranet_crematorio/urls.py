2a49323e75c79e897c96bdcbfd07368df9492ac0
RenatoYnonan <renatoignite1122@gmail.com>
2025-01-24 18:07:38 -0500

    Cambios 24-01-25

--- CREMATORIOVN/intranet_crematorio/urls.py
+++ CREMATORIOVN/intranet_crematorio/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from intranet_crematorio.views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns= [
-    path('intranet/', Intranet.as_view(), name='intranet')
+    path('intranet/', Intranet.as_view(), name='intranet'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
 
--- CREMATORIOVN/intranet_crematorio/views.py
+++ CREMATORIOVN/intranet_crematorio/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import *
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
-class Intranet(TemplateView):
+class Intranet(LoginRequiredMixin,TemplateView):
+    login_url = '/admin/'
     template_name = 'index.html'
