0555d7790ccc31971c410d33d19e000e9da93e68
RenatoYnonan <renatoignite1122@gmail.com>
2025-01-31 16:58:43 -0500

    cambios 31-01-2025

--- CREMATORIOVN/base/views.py
+++ CREMATORIOVN/base/views.py
@@ -18,7 +18,7 @@ class PaginaPrincipal(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
-        context['planes'] =  ModelsPlanes.objects.all()
+        context['planes'] =  ModelsPlanes.objects.prefetch_related('planes').all()
         return context
 
 class MuralRecuerdo(ListView):
